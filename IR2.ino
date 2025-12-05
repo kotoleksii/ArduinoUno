ec50baa75d90c40c8e577dc4425e04ef2e072e2a
kotoleksii <66323597+kotoleksii@users.noreply.github.com>
2025-12-05 17:52:32 +0200

    Update IR2.ino

--- IR2.ino
+++ IR2.ino
@@ -1,11 +1,15 @@
 #include <IRremote.h>
 
 const int IR_RECEIVER_PIN = 4;
+const int LED_PIN = 11;
 
 void setup()
 {
   Serial.begin(9600);                 
-  IrReceiver.begin(IR_RECEIVER_PIN);  
+  IrReceiver.begin(IR_RECEIVER_PIN); 
+  
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
 }
 
 void loop()
@@ -17,7 +21,17 @@ void loop()
       Serial.print("Decoded Data: ");
       Serial.println(signalCode, HEX);                      
     }
+    
+     if(signalCode == 0xFF00BF00) {
+      digitalWrite(LED_PIN, HIGH);
+      Serial.println("LED turned ON");
+      delay(50);
+    }  else {
+      digitalWrite(LED_PIN, LOW);
+      Serial.println("LED turned OFF");
+      delay(50);
+    }
 
-    IrReceiver.resume();
+    IrReceiver.resume();                                    
   }
 }
